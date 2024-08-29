f4cff0a0757737b8ef4bb3a5f76d565bd17fa4f5
Ahmad_bankconnect <shahram@bankconnect.io>
2024-08-30 02:18:54 +0500

    Order page listing and also integrated filter also

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\CustomerController::class, 'index'])->name('customer.index');
+Route::get('/', [\App\Http\Controllers\OrderController::class, 'index'])->name('customer.index');
